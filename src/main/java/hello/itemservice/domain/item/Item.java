dea89ad3cbd9fd24e2d932b77602e5174c5e1082
Cat Hanbit <tlsgus92wns@naver.com>
2023-01-02 00:47:49 +0900

    feat/v4 commit

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,15 +12,15 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총압이 10000원 넘게 입력해주세요.")
 public class Item {
 
-    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
+//    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
-    @NotBlank(message = "공백 X", groups = {SaveCheck.class, UpdateCheck.class})
+//    @NotBlank(message = "공백 X", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Range(min = 1000, max = 1000000)
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000)
     private Integer price;
-    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
-    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 요구사항 추가
+//    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+//    @Max(value = 9999, groups = {SaveCheck.class}) // 수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
